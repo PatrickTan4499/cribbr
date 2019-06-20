fd660a656c0d9c463128d8217f2dfbc7b3f36af5
Patrick Tan <patrick4499@gmail.com>
2019-06-19 20:29:41 -0700

    starting firebase

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from '../navbar/navbar';
+import Navbar from '../Navbar/navbar';
 
 function App() {
   return (

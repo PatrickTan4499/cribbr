0a265b20372a299402a534732f718695fc16c001
Patrick Tan <patrick4499@gmail.com>
2019-06-21 13:39:12 -0700

    finish accountAuth

--- src/components/SignIn/signIn.js
+++ src/components/SignIn/signIn.js
@@ -5,15 +5,79 @@ import { compose } from 'recompose';
 import { SignUpLink } from '../SignUp/signUp';
 import { PasswordForgetLink } from '../PasswordForget/passwordForget';
 import { withFirebase } from '../Firebase';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core' 
+import blue from '@material-ui/core/colors/blue'
+
 import * as ROUTES from '../../constants/routes';
 
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    align: 'center',
+
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 const SignInPage = () => (
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm />
-    <PasswordForgetLink />
-    <SignUpLink />
-  </div>
+  
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+      <div align="center">
+        <Avatar className={useStyles.avatar} color="primary" align="center">
+            <LockOutlinedIcon />
+        </Avatar>
+      </div>
+        <Typography component="h1" variant="h5" align="center">
+            Sign in
+        </Typography>
+        <SignInForm />
+        <Grid container align="left">
+            <Grid item xs align="left">
+              <PasswordForgetLink />
+            </Grid>
+            <Grid item>
+                <SignUpLink />
+            </Grid>
+          </Grid>
+        
+        
+ 
+  </Container>
 );
 
 const INITIAL_STATE = {
@@ -28,7 +92,6 @@ class SignInFormBase extends Component {
 
     this.state = { ...INITIAL_STATE };
   }
-
   onSubmit = event => {
     const { email, password } = this.state;
 
@@ -56,24 +119,52 @@ class SignInFormBase extends Component {
 
     return (
       <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
+              <MuiThemeProvider theme={theme}>
+        <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
 
+            autoComplete="email"
+            autoFocus
+            name="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+        />
+        <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Password"
+
+            autoComplete="password"
+            autoFocus
+            name="password"
+            value={password}
+            onChange={this.onChange}
+            type="password"
+        />
+
+        {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+        /> */}
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={isInvalid}
+          >
+            Sign In
+        </Button>
+        </MuiThemeProvider>
         {error && <p>{error.message}</p>}
       </form>
     );
--- src/components/SignUp/signUp.js
+++ src/components/SignUp/signUp.js
@@ -1,15 +1,70 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core' 
+import blue from '@material-ui/core/colors/blue'
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 const SignUpPage = () => (
-  <div>
-    <h1>SignUp</h1>
-    <SignUpForm />
-  </div>
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div align="center">
+      <Avatar className={useStyles.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      </div>
+      <Typography component="h1" variant="h5" align='center'>
+        Sign up
+      </Typography> 
+      <br></br>    
+        <SignUpForm />
+
+  </Container>
 );
 
 const INITIAL_STATE = {
@@ -34,23 +89,26 @@ class SignUpFormBase extends Component {
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         // Create a user in your Firebase realtime database
-        return this.props.firebase
+        this.props.firebase
           .user(authUser.user.uid)
           .set({
             username,
             email,
+          })
+          .then(() => {
+            this.setState({ ...INITIAL_STATE });
+            this.props.history.push(ROUTES.HOME);
+          })
+          .catch(error => {
+            this.setState({ error });
           });
-      })
-      .then(authUser => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.HOME);
       })
       .catch(error => {
         this.setState({ error });
       });
 
     event.preventDefault();
-  }
+  };
 
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -73,38 +131,83 @@ class SignUpFormBase extends Component {
 
     return (
       <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Full Name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="passwordOne"
-          value={passwordOne}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <input
-          name="passwordTwo"
-          value={passwordTwo}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Confirm Password"
-        />
-        <button disabled={isInvalid} type="submit">Sign Up</button>
-
-        {error && <p>{error.message}</p>}
+        <MuiThemeProvider theme={theme}>
+        <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="username"
+            name="username"
+            variant="outlined"
+            required
+            fullWidth
+            id="username"
+            label="Full Name"
+            autoFocus
+            value={username}
+            onChange={this.onChange}
+            type="text"
+            placeholder="Full Name"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={this.onChange}
+            type="text"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="passwordOne"
+            label="Password"
+            type="passwordOne"
+            id="passwordOne"
+            autoComplete="passwordOne"
+            value={passwordOne}
+            onChange={this.onChange}
+            type="password"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="passwordTwo"
+            label="Confirm Password"
+            type="passwordTwo"
+            id="passwordTwo"
+            autoComplete="passwordTwo"
+            value={passwordTwo}
+            onChange={this.onChange}
+            type="password"
+          />
+        </Grid>
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={useStyles.submit}
+            disabled={isInvalid}
+          >
+            Sign Up
+        </Button>
+          {error && <p>{error.message}</p>}
+        </Grid>
+        </MuiThemeProvider>
       </form>
+      
     );
   }
 }
@@ -115,10 +218,7 @@ const SignUpLink = () => (
   </p>
 );
 
-const SignUpForm = compose(
-  withRouter,
-  withFirebase,
-)(SignUpFormBase);
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 
 export default SignUpPage;
 

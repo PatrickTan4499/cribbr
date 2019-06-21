803f13188886e6b104df936b8bed0a546df47bdb
RaymondLin <raymond.lin0226@gmail.com>
2019-06-20 19:39:09 -0700

    merge conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from '../navbar/navbar';
+import Navbar from '../Navbar/navbar';
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
+
+
+import Navigation from '../Navigation/navigation';
+import LandingPage from '../Landing/landing';
+import SignUpPage from '../SignUp/signUp';
+import SignInPage from '../SignIn/signIn';
+import PasswordForgetPage from '../PasswordForget/passwordForget';
+import HomePage from '../Home/home';
+import AccountPage from '../Account/account';
+import AdminPage from '../Admin/admin';
+
+import * as ROUTES from '../../constants/routes';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <div>
+        <Navigation />
+
+              <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
+      </Router>
+
     </div>
   );
 }

6fc8142741f87259414f0a49631fc93430381b33
RaymondLin <raymond.lin0226@gmail.com>
2019-08-04 22:55:42 -0700

    Revert "working on modals for calendar events, popover for calendar cells"
    This reverts commit 746189d4d07cac5d70dc129086144a2e34d8b091.

--- src/components/Calendar/calendarEventModal.js
+++ src/components/Calendar/calendarEventModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Button from '@material-ui/core/Button';
@@ -10,7 +12,6 @@ import { Typography } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import { app } from '../Firebase/firebase';
-import { KeyboardDatePicker } from '@material-ui/pickers';
 
 class CalendarEventModal extends Component
 {
@@ -24,30 +25,16 @@ class CalendarEventModal extends Component
         this.state = {
             choreName: '',
             timestamp: this.props.selectedDate.format('x'),
-            error: false,
         };
     }
 
     addEvent = () => {
-        if (!this.state.choreName.replace(/\s/g, '').length)
-        {
-            this.setState({
-                error: true,
-            })
-        }
-        else
-        {
-            this.eventsRef.add({
-                choreName: this.state.choreName,
-                timestamp: this.state.timestamp,
-            });
-            this.props.handleClose();
-            this.props.reload();
-            this.setState ({
-                choreName: '',
-                error: false,
-            });
-        }
+        this.eventsRef.add({
+            choreName: this.state.choreName,
+            timestamp: this.state.timestamp,
+        });
+        this.props.handleClose();
+        this.props.reload();
     }
     
     handleNameInput(e)
@@ -62,49 +49,35 @@ class CalendarEventModal extends Component
         return (
             <Modal open={this.props.modalOpen} onClose={this.props.handleClose} overflow="scroll">
                 <Paper className="paper">
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Typography variant="h6" id="modal-title">
-                                Add Chore
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography>Chore Name</Typography>
-                            <TextField 
-                                style={{ width: '100%' }}
-                                required label="Required"
-                                onChange={this.handleNameInput}
-                                error={this.state.error}
-                                />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography>Repeats</Typography>
-                            <FormControl component="fieldset">
-                                <RadioGroup row>
-                                    <FormControlLabel value="daily" control={<Radio color="primary"/>} label="Daily" />
-                                    <FormControlLabel value="weekly" control={<Radio color="primary"/>} label="Weekly" />
-                                    <FormControlLabel value="monthly" control={<Radio color="primary"/>} label="Monthly" />
-                                    <FormControlLabel value="none" control={<Radio color="primary"/>} label="None" />
-                                </RadioGroup>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button variant="contained" color="primary" onClick={(e) => this.addEvent()} size="small">
-                                Submit
-                            </Button> 
-                        </Grid>
-                        <Grid item xs={12}>
-                            <KeyboardDatePicker
-                                margin="normal"
-                                id="mui-pickers-date"
-                                label="Date picker"
-                                value={selectedDate}
-                                onChange={handleDateChange}
-                                KeyboardButtonProps={{
-                                    'aria-label': 'change date',
-                                }}
-                            />
-                        </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant="h6" id="modal-title">
+                            Add Chore
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>Chore Name</Typography>
+                        <TextField 
+                            style={{ width: '100%' }}
+                            required label="Required"
+                            onChange={this.handleNameInput}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>Repeats</Typography>
+                        <FormControl component="fieldset">
+                            <RadioGroup row>
+                                <FormControlLabel value="daily" control={<Radio color="primary"/>} label="Daily" />
+                                <FormControlLabel value="weekly" control={<Radio color="primary"/>} label="Weekly" />
+                                <FormControlLabel value="monthly" control={<Radio color="primary"/>} label="Monthly" />
+                                <FormControlLabel value="none" control={<Radio color="primary"/>} label="None" />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant="contained" color="primary" onClick={(e) => this.addEvent()} size="small">
+                            Submit
+                        </Button> 
+                    </Grid>
                     </Grid> 
                 </Paper>
             </Modal>
